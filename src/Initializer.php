5dba43cc32f264008a9a84395cb054a3fa19eeab
Niko Hujanen <niko.hujanen@gmail.com>
2026-03-31 23:36:33 +0300

    feat: initializer

--- src/Initializer.php
+++ src/Initializer.php
@@ -2,6 +2,7 @@
 
 namespace Phi;
 
+use Composer\Factory;
 use Composer\Script\Event;
 
 abstract class Initializer
@@ -11,9 +12,46 @@ abstract class Initializer
         $io = $event->getIO();
         $io->write('Initializing project...');
 
-        while (($authorName = $io->ask('Author name: ')) === '');
-        while (($authorEmail = $io->ask('Author email: ')) === '');
+        while (!($authorName = $io->ask('Author name: ')));
+        while (!($authorEmail = $io->ask('Author email: ')));
 
-        $io->write("Hello, {$authorName} ({$authorEmail})!");
+        $file = Factory::getComposerFile();
+        $path = dirname(realpath($file));
+        $json = json_decode(file_get_contents($file), true);
+
+        $io->write('- Modyfing author...');
+
+        $json['authors'] = [
+            [
+                'name' => $authorName,
+                'email' => $authorEmail,
+            ],
+        ];
+
+        $io->write('- Removing unnecessary dependencies...');
+
+        unset($json['require-dev']['composer/composer']);
+
+        $io->write('- Removing unnecessary scripts...');
+
+        unset($json['scripts']['post-create-project-cmd']);
+
+        $io->write('- Saving new composer.json...');
+
+        file_put_contents($file . '.new', json_encode($json, JSON_PRETTY_PRINT));
+
+        $io->write('- Modify LICENSE...');
+
+        $licenseFile = $path . DIRECTORY_SEPARATOR . 'LICENSE';
+        $license = file($licenseFile);
+        $license[0] = sprintf("Copyright %d %s\n", date('Y'), $authorName);
+        file_put_contents($licenseFile . '.new', implode("", $license));
+
+        $io->write('- Unlink initializer...');
+
+        @unlink(__FILE__);
+
+        $io->write('- Done!');
+
     }
 }
