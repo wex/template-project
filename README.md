7cf6b5b4f08aab9a710b8edccd3755ecc3daf984
Niko Hujanen <niko.hujanen@gmail.com>
2026-04-01 00:11:01 +0300

    feat: validators, better io, git init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Template project with Composer, Pest, Pint and default settings
+
+This initializes the project with the following features:
+- Composer
+- Pest
+- Pint
+- Default settings
+
+## Usage
+
+`composer create-project phi/template-project <target-path>` will create new `target-path` directory in current location with initialized project.
--- src/Initializer.php
+++ src/Initializer.php
@@ -7,15 +7,46 @@ use Composer\Script\Event;
 
 abstract class Initializer
 {
+    public static function validateVendor(?string $value): bool
+    {
+        return (bool) preg_match('/^[a-z0-9]([_.-]?[a-z0-9]+)*$/i', (string) $value);
+    }
+
+    public static function validateName(?string $value): bool
+    {
+        return true;
+    }
+
+    public static function validateEmail(?string $value): bool
+    {
+        return strpos((string) $value, '@') !== false;
+    }
+
     public static function run(Event $event)
     {
         $io = $event->getIO();
         $io->write('Initializing project...');
 
-        while (!($vendorName = $io->ask('Vendor name: ')));
-        while (!($packageName = $io->ask('Package name: ')));
-        while (!($authorName = $io->ask('Author name: ')));
-        while (!($authorEmail = $io->ask('Author email: ')));
+        $vendorName = $io->askAndValidate('Vendor       : ', function ($value) {
+            if (!static::validateVendor($value)) {
+                throw new \Exception('Invalid vendor name');
+            }
+        });
+        $packageName = $io->askAndValidate('Package      : ', function ($value) {
+            if (!static::validateVendor($value)) {
+                throw new \Exception('Invalid package name');
+            }
+        });
+        $authorName = $io->askAndValidate('Author name  : ', function ($value) {
+            if (!static::validateName($value)) {
+                throw new \Exception('Invalid author name');
+            }
+        });
+        $authorEmail = $io->askAndValidate('Author email : ', function ($value) {
+            if (!static::validateEmail($value)) {
+                throw new \Exception('Invalid author email');
+            }
+        });
 
         $file = Factory::getComposerFile();
         $path = dirname(realpath($file));
@@ -53,6 +84,14 @@ abstract class Initializer
         @unlink(__FILE__);
 
         $io->write('- Done!');
+
+        $git = $io->askConfirmation('Initialize Git repository (y/N) : ', false);
+
+        if ($git) {
+            $io->write('- Initialize Git repository...');
+            @exec('git init');
+            @exec('git add .');
+        }
 
     }
 }
